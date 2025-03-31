26fd7ad8b40a07ab5a3578d51c46c47d94b0c1b6
Shreyansh Agrawal <shreyansh.agrawal@metacube.com>
2025-03-31 10:00:32 +0530

    added: Session1 Question2

---
+++ Session1/Question2.java
@@ -0,0 +1,35 @@
+package Session1; 
+
+class Node{
+    int data; 
+    Node next; 
+    Node(int val){
+        this.data = val; 
+    }
+}
+
+public class Question2 {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = head.next;
+
+        if(detectLoop(head))System.out.println("true");
+        else System.out.println("false");
+    }
+    
+    public static boolean detectLoop(Node head){
+        if(head==null)return false; 
+        Node slow = head,fast = head; 
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next; 
+            fast = fast.next.next; 
+            if(slow == fast)return true; 
+        }
+        return false; 
+    }
+    
+
+}
