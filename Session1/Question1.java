583fc90bc8f09cc7fc4864fbed1c416d58e9ec43
Shreyansh Agrawal <shreyansh.agrawal@metacube.com>
2025-04-07 20:57:12 +0530

    added: terminal mode in Session1 Question1

--- Session1/Question1.java
+++ Session1/Question1.java
@@ -1,7 +1,10 @@
-package Session1; 
+package Session1;
+
+import Utility.Utils;
+import java.util.Scanner;
 
 public class Question1 {
-    
+
     static class Node {
         int data;
         Node next;
@@ -10,7 +13,7 @@ public class Question1 {
             this.next = null;
         }
     }
-    
+
     public static void printList(Node head) {
         Node current = head;
         while (current != null) {
@@ -20,38 +23,58 @@ public class Question1 {
         System.out.println("null");
     }
 
+    // rotating the list
+    public static Node rotateSubList(Node head, int L, int R, int N) {
+        if (head == null) return head;
+        Node a = head;
+        for (int i = 0; i < L - 2; i++) {
+            a = a.next;
+        }
+        Node b = a;
+        for (int i = 0; i < N; i++) {
+            b = b.next;
+        }
+        Node c = head;
+        for (int i = 0; i < R - 1; i++) {
+            c = c.next;
+        }
+        Node d = c.next;
+        c.next = a.next;
+        a.next = b.next;
+        b.next = d;
+        return head;
+    }
+
     public static void main(String[] args) {
-        Node head = new Node(2);
-        head.next = new Node(3);
-        head.next.next = new Node(4);
-        head.next.next.next = new Node(5);
-        head.next.next.next.next = new Node(6);
-        head.next.next.next.next.next = new Node(7);
+        Scanner sc = new Scanner(System.in);
+
+        // Step 1: Get Linked List Input
+        System.out.println("Enter the number of nodes in the linked list:");
+        int numNodes = Utils.inputNaturalNumber(sc);
+        System.out.println("Enter the values of the nodes:");
+        Node head = new Node(Utils.inputNaturalNumber(sc));
+        Node current = head;
+        for (int i = 1; i < numNodes; i++) {
+            current.next = new Node(Utils.inputNaturalNumber(sc));
+            current = current.next;
+        }
+
         System.out.println("Original Linked List:");
         printList(head);
-        head = rotateSubList(head, 2,5,2);
+
+        // Step 2: Get Input for L, R, N
+        System.out.println("Enter the value of L (start index of the sublist):");
+        int L = Utils.inputNumberInRange(sc, 1, numNodes);
+        System.out.println("Enter the value of R (end index of the sublist):");
+        int R = Utils.inputNumberInRange(sc, L, numNodes);
+        System.out.println("Enter the value of N (number of rotations):");
+        int N = Utils.inputNumberInRange(sc, 0, R - L + 1);
+
+        // Step 3: Modify Linked List
+        head = rotateSubList(head, L, R, N);
         System.out.println("Modified Linked List:");
         printList(head);
-    }
-    
-    public static Node rotateSubList(Node head, int L, int R, int N) {
-        if(head == null)return head; 
-        Node a = head;  
-        for(int i = 0;i<L-2;i++){
-            a = a.next; 
-        }
-        Node b = a; 
-        for(int i = 0;i<N;i++){
-            b =  b.next; 
-        }
-        Node c = head; 
-        for(int i = 0;i<R-1;i++){
-            c = c.next; 
-        }
-        Node d = c.next; 
-        c.next = a.next; 
-        a.next = b.next; 
-        b.next = d; 
-        return head; 
+
+        sc.close();
     }
 }
